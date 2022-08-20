b91ff8b56622184d8b80ccf9952ba58c2ee9d63a
Mary <maryqueenk1@gmail.com>
2022-08-20 19:33:56 +0300

    добавление задачи 23 с решением

---
+++ HW3Ex03/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите метод, который принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.Clear();
+Console.WriteLine("Задача 23");
+
+Console.Write("Введите любое число от 1: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+void Cube(int number)
+{
+    for(int V = 1; V <= number; V++)
+    {
+        int K = V * V * V;
+        Console.Write($"{K} ");
+    }
+}
+
+Cube(N);
